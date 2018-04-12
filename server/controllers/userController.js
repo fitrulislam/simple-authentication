7383abd53d51fb96c6f967978bdf493a611cf1ca
Fitrul Islam <fitrul.islam@gmail.com>
2018-04-12 14:23:04 +0700

    add signin

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
   signup: (req,res)=>{
@@ -17,6 +18,28 @@ module.exports = {
       })
   },
   signin: (req,res)=>{
-
+    User.find({
+      username: req.body.obj.username
+    })
+    .then(user=>{
+      if(user){
+        if(user[0].password == req.body.obj.password){
+          let token = jwt.sign({id: user[0]._id}, 'menguasai dunia')
+          res.status(200).json({
+            message: `this is user`,
+            token: token
+          })
+        }
+      } else {
+        res.status(400).json({
+          message: `User not found`
+        })
+      }
+    })
+    .catch(err=>{
+      res.status(500).json({
+        message: 'error'
+      })
+    })
   }
 }
